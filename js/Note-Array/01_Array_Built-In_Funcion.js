5124007b4d36b9919f02799f072ed4300e69e77d
JangHwanPark <wkd5878@gmail.com>
2024-03-06 22:13:52 +0900

    Built-In Fn, Exception Handling 초안

---
+++ js/Note-Array/01_Array_Built-In_Funcion.js
@@ -0,0 +1,46 @@
+/**
+ * Built-in Function
+ * 프로그래밍에서 제공하는 내장함수 (프로그래밍 언어의 표준 라이브러리의 일부로 재공되고,
+ * 개발자가 별도로 정의(만들지 않고)하지 않고 바로 사용할 수 있다.
+ */
+const users = [
+    { id: 1, name: "Alice", age: 28, isActive: true },
+    { id: 2, name: "Bob", age: 23, isActive: false },
+    { id: 3, name: "Charlie", age: 30, isActive: true },
+    { id: 4, name: "David", age: 22, isActive: false },
+    { id: 5, name: "Eve", age: 35, isActive: true },
+];
+
+// map() -
+
+// filter() -
+
+// reduce() -
+
+// forEach() -
+
+// find() -
+
+// findIndex() -
+
+// every() -
+
+// some() -
+
+// sort() -
+
+// slice() -
+
+// concat() -
+
+// join() -
+
+// indexOf() -
+
+// lastIndexOf() -
+
+// includes() -
+
+// flat() -
+
+// flatMap() -
