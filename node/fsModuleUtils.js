17dcdfd85e253ce20bf44ec52d9a9f77770f1392
japark <wkd5878@gmail.com>
2024-04-03 17:27:06 +0900

    입출력 시스템 (디버깅 필요)

--- node/fsModuleUtils.js
+++ node/fsModuleUtils.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+const readline = require("readline");
+
+const TEST_INPUT_TEXT = '../../data/inputFile.txt';
+let userInput = readline.createInterface({
+    input: TEST_INPUT_TEXT,
+    output: TEST_INPUT_TEXT
+})
+
+oninput.on('line', function (line)  {
+    console.log(line);
+    userInput.close();
+})
+
 const fsInFile = () => {
     let data = [1];
     return `data: ${data}`;
